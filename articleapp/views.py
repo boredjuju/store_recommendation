c9050e87837a2b3d0e18d72a46dc9a3b4627d9d4
Hyeonju Cha <hy9498@gmail.com>
2021-11-26 18:56:46 +0900

    2021/11/26_1

--- articleapp/views.py
+++ articleapp/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from articleapp.models import history
 
 
 def hello_world(request):
@@ -11,11 +12,33 @@ def index(request):
 
     if request.method == 'POST':
 
-        temp = request.POST.get('addr_input')
+        addr_input = request.POST.get('addr_input')
+        region_input = request.POST.get('region_input')
+        menu_input = request.POST.get('menu_input')
 
-        return render(request, 'articleapp/index.html', context={'text': 'POST METHOD!!!'})
+        new_history = history()
+        new_history.menu = menu_input
+        new_history.addr = addr_input
+        new_history.region = region_input
+        new_history.save()
+
+        return render(request, 'articleapp/search.html', context={'history_output': new_history})
     else:
-        return render(request, 'articleapp/index.html', context={'text': 'GET METHOD!!!'})
+        return render(request, 'articleapp/index.html')
 
 def search(request):
-    return render(request, 'articleapp/search.html')
+    if request.method == 'POST':
+
+        addr_input = request.POST.get('addr_input')
+        region_input = request.POST.get('region_input')
+        menu_input = request.POST.get('menu_input')
+
+        new_history = history()
+        new_history.menu = menu_input
+        new_history.addr = addr_input
+        new_history.region = region_input
+        new_history.save()
+
+        return render(request, 'articleapp/search.html', context={'history_output': new_history})
+    else:
+        return render(request, 'articleapp/search.html')
--- store_recommendation/settings.py
+++ store_recommendation/settings.py
@@ -97,6 +97,13 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db_test.sqlite3',
+#     }
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
